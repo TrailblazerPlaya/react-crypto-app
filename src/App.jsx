f7ca0848713ca6de666ef5188db17eb7581a6020
fornara <fornarapro@gmail.com>
2024-01-31 19:34:07 +0300

    Add Header | Modal | Drawer

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import AppLayout from "./components/Layout/AppLayout";
+import { CryptoContextProvider } from "./context/crypto-context";
+export default function App() {
+	return (
+		<CryptoContextProvider>
+			<AppLayout />
+		</CryptoContextProvider>
+	);
+}
